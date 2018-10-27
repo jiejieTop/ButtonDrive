8f783431981742a4b3d79628d3c0b252de7ac8b6
jiejieTop <1161959934@qq.com>
2018-10-27 18:32:37 +0800

    pkg

--- include.h
+++ include.h
@@ -2,12 +2,12 @@
 #define __INCLUDE_H
 
 /* c 标准库 */
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdint.h>
 
 /* 重定义头文件 */
-#include "redef.h"
+//#include "redef.h"
 
 /* debug头文件 */
 #include "debug.h"
